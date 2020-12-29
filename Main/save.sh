7943f086af221346abb22013ec09f80640c43d66
Shalva Lekishvili <60827521+s-lekishvili-s@users.noreply.github.com>
2020-12-30 01:35:39 +0400

    Add files via upload

---
+++ Main/save.sh
@@ -0,0 +1,31 @@
+cyn='\e[0;36m'
+
+lgrn='\e[1;32m'
+
+r='\e[1;31m'
+
+y='\e[1;33m'
+################
+clear
+echo
+echo
+echo
+echo '
+
+
+                    _  _  _____  ____  ____
+                   ( \( )(  _  )(_  _)( ___)
+                    )  (  )(_)(   )(   )__)
+                   (_)\_)(_____) (__) (____) ' | Maincat
+echo " "
+echo " "
+cat save.txt
+echo " "
+echo "                      " Be polite buddy | Maincat
+echo " "
+echo " "
+sleep 50.0
+clear
+cd $HOME/Android_Infection
+bash Android_Infection.sh
+exit
